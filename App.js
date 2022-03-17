4a7ac4d41deddd606dafdf09dfb5f145f0c81a2f
Anna Kastet <anna.kastet@eika.no>
2022-03-17 17:23:17 +0100

    Add PoC of Webview issue

--- App.js
+++ App.js
@@ -1,11 +1,34 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {Button, StyleSheet, Text, View} from 'react-native';
+import {useState} from "react";
+import { WebView } from "react-native-webview";
 
 export default function App() {
+  const PREPROD_URI = "insert here"
+  const PROD_URI = "insert here"
+
+  const [webViewProps, setPreprod] = useState({show: false})
+
+  const resetViews = () => { setPreprod({show: false }) }
+
+  console.log(webViewProps)
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Button onPress={() => resetViews()} title="Go back" />
+        {
+            webViewProps.show ? <WebView
+                    originWhitelist={['*']}
+                    style={{flex: 1, backgroundColor: 'blue'}}
+                    source={{ uri: webViewProps.uri }}
+                /> :
+                <View>
+                    <Text>Open up App.js to start working on your app!</Text>
+                    <StatusBar style="auto"/>
+                    <Button onPress={() => setPreprod({show: true, uri: PREPROD_URI})} title="Click for Preprod"/>
+                    <Button onPress={() => setPreprod({show: true, uri: PROD_URI})} title="Click for Prod"/>
+                </View>
+        }
+
     </View>
   );
 }
@@ -13,8 +36,6 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#fff'
   },
 });
