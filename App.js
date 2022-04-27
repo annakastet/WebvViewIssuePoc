52872765b6b17deb0172295850560049ae41e90f
Anna Kastet <anna.kastet@eika.no>
2022-04-27 08:51:27 +0200

    Test inappbrowser (custom tabs)

--- App.js
+++ App.js
@@ -1,31 +1,40 @@
-import { StatusBar } from 'expo-status-bar';
-import {Button, StyleSheet, Text, View} from 'react-native';
+import {StatusBar} from 'expo-status-bar';
+import {Alert, Button, StyleSheet, Text, View} from 'react-native';
 import {useState} from "react";
-import { WebView } from "react-native-webview";
+
+import {InAppBrowser} from 'react-native-inappbrowser-reborn'
 
 export default function App() {
   const PREPROD_URI = "insert here"
   const PROD_URI = "insert here"
 
-  const [webViewProps, setPreprod] = useState({show: false})
+  const [webViewProps, setWebView] = useState({show: false})
 
-  const resetViews = () => { setPreprod({show: false }) }
+  const resetViews = () => { setWebView({show: false }) }
+
+  async function openBrowser(url) {
+    try {
+      if (await InAppBrowser.isAvailable()) {
+        const result = InAppBrowser.open(url)
+        Alert.alert(JSON.stringify(result))
+      } else {
+        console.log("In app browser not available")
+      }
+    } catch (error) {
+      Alert.alert(error.message)
+    }
+  }
 
   console.log(webViewProps)
+  if (webViewProps.show) { openBrowser(webViewProps.uri).then(() => resetViews()) }
   return (
     <View style={styles.container}>
-      <Button onPress={() => resetViews()} title="Go back" />
         {
-            webViewProps.show ? <WebView
-                    originWhitelist={['*']}
-                    style={{flex: 1, backgroundColor: 'blue'}}
-                    source={{ uri: webViewProps.uri }}
-                /> :
                 <View>
                     <Text>Open up App.js to start working on your app!</Text>
                     <StatusBar style="auto"/>
-                    <Button onPress={() => setPreprod({show: true, uri: PREPROD_URI})} title="Click for Preprod"/>
-                    <Button onPress={() => setPreprod({show: true, uri: PROD_URI})} title="Click for Prod"/>
+                    <Button onPress={() => setWebView({show: true, uri: PREPROD_URI})} title="Click for Preprod"/>
+                    <Button onPress={() => setWebView({show: true, uri: PROD_URI})} title="Click for Prod"/>
                 </View>
         }
 
